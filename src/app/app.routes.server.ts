87502e90225208d0758f004c40d09766efe39ff1
JeraLaunchControl <jera@launchcontrol.us>
2026-06-03 11:05:58 +0200

    Prerender fix

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,8 +1,26 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
-  {
-    path: '**',
-    renderMode: RenderMode.Prerender
-  }
+  { path: '', renderMode: RenderMode.Prerender },
+
+  { path: 'vesti', renderMode: RenderMode.Prerender },
+  { path: 'vesti/nova', renderMode: RenderMode.Prerender },
+  { path: 'vesti/:id', renderMode: RenderMode.Server },
+  { path: 'vesti/:id/uredi', renderMode: RenderMode.Server },
+
+  { path: 'afere', renderMode: RenderMode.Prerender },
+  { path: 'afere/nova', renderMode: RenderMode.Prerender },
+  { path: 'afere/:id', renderMode: RenderMode.Server },
+  { path: 'afere/:id/uredi', renderMode: RenderMode.Server },
+
+  { path: 'akcije', renderMode: RenderMode.Prerender },
+  { path: 'akcije/nova', renderMode: RenderMode.Prerender },
+  { path: 'akcije/:id', renderMode: RenderMode.Server },
+  { path: 'akcije/:id/uredi', renderMode: RenderMode.Server },
+
+  { path: 'kalendar', renderMode: RenderMode.Prerender },
+  { path: 'nalog', renderMode: RenderMode.Server },
+  { path: 'prijava', renderMode: RenderMode.Prerender },
+
+  { path: '**', renderMode: RenderMode.Server },
 ];
